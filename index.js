8da5178f34d40340ec16b5697d0f9542cdab3fbf
Divine Patrick <132227658+divinepatrick@users.noreply.github.com>
2024-11-25 21:56:33 +0100

    Update index.js

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ const newQuoteButton = document.getElementById('new-quote');
 async function getRandomQuote() {
   try {
     const response = await fetch('https://api.quotable.io/random');
-    const data = await response.json('');
+    const data = await response.json("");
     return data;
   } catch (error) {
     console.error('Error fetching quote:', error);
